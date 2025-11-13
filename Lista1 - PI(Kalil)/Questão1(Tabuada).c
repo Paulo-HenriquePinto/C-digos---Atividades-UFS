7e5b38fcd5382573695fd3876e79f358ab782f0a
Paulo-HenriquePinto <paulo.pinto@dcomp.ufs.br>
2025-11-12 23:30:10 -0300

    Create Questão1(Tabuada).c

---
+++ Lista1 - PI(Kalil)/Questão1(Tabuada).c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int número;
+
+int main(){
+  
+    scanf("%d", &número);
+
+    int a1 = número*1;
+    int a2 = número*2; 
+    int a3 = número*3; 
+    int a4 = número*4; 
+    int a5 = número*5; 
+    int a6 = número*6; 
+    int a7 = número*7; 
+    int a8 = número*8; 
+    int a9 = número*9; 
+    
+    
+    printf("%d X 1 = %d\n",número,a1 );
+    printf("%d X 2 = %d\n",número,a2);
+    printf("%d X 3 = %d\n",número,a3);
+    printf("%d X 4 = %d\n",número,a4);
+    printf("%d X 5 = %d\n",número,a5);
+    printf("%d X 6 = %d\n",número,a6);
+    printf("%d X 7 = %d\n",número,a7);
+    printf("%d X 8 = %d\n",número,a8);
+    printf("%d X 9 = %d\n",número,a9);
+    return 0;
+}
